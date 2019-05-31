20fa4a09708ae346c627fba901e8fa817c4b5c13
Szymon Hanzel <szyhan@gmail.com>
2019-05-31 23:50:21 +0200

    add checkActivity

---
+++ app/src/main/java/com/example/proovit/CheckActivity.java
@@ -0,0 +1,16 @@
+package com.example.proovit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+
+    }
+
+}
